75430dd9332b64da8340a0aa8d1b5103a06e68f8
Priyesh Soni <priyesh9792@gmail.com>
2023-11-10 20:19:42 +0530

    Creating new React Native project , Running on Emulator and Changes On App.tsx file

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,30 @@
+
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App=()=>{
+  return (
+    <Text style={{fontSize:30}}> Hello I am Priyesh</Text>
+  )
+}
+
+export default App;
